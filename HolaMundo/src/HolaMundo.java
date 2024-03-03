7bd5c830ad3f657a67a7cb2bcb2d9675c77dd410
Isabel Lopez Martin <isabel.lopezmartin@educa.madrid.org>
2024-03-03 13:05:01 +0100

    2 cambio en Eclipse

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -6,6 +6,7 @@ public class HolaMundo {
 		System.out.println("Hola Mundo");
 		System.out.println("Modifico la clase HolaMundo");
 		System.out.println("Modifico la clase HolaMundo desde Eclipse");
+		System.out.println("2 Modifico la clase HolaMundo desde Eclipse");
 
 	}
 
